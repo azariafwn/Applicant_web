b037d5bd370ff3975e8fe0382b16842748f28b22
BIMOUW <bimonurika@gmail.com>
2025-07-31 19:45:47 +0700

    Add project files.

---
+++ LearningApplicantWeb/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningApplicantWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
